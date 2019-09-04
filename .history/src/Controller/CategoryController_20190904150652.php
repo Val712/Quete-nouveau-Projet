bba8eb6d861afd1025e934f42a327cf23563ec6d
val712 <valerie.burguet@gmail.com>
2019-09-04 17:08:29 +0200

    quete 11 générer un Crud

---
+++ .history/src/Controller/CategoryController_20190904150652.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+ /**
+    * @Route("/category", name="category_")
+   */
+
+
+class CategoryController extends AbstractController
+{
+   /**
+    * @Route("/add", name="add")
+   */
+  public function add(Request $request)
+  {
+    $category = new Category();
+    $addcat = $this->createForm(
+      CategoryType::class, 
+      $category,
+      ['method'=> Request::METHOD_POST]
+       );
+
+       $addcat->handleRequest($request);
+       if ($addcat->isSubmitted() && $addcat->isValid()) {
+        $data = $addcat->getData();
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($data);
+        $entityManager->flush();
+
+
+       }
+
+
+      return $this->render('blog/addCategory.html.twig',['addcat' => $addcat->createView()]);
+  }
+
+
+}
